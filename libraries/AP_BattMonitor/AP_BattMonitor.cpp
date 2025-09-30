16d34b1211f445b9eacd2591b4fbecb372bd6b58
Andrew Gregg <agregg18@gmail.com>
2025-09-29 20:54:20 -0400

    Initial commit of VE.Direct MPPT battery monitor driver

--- libraries/AP_BattMonitor/AP_BattMonitor.cpp
+++ libraries/AP_BattMonitor/AP_BattMonitor.cpp
@@ -28,6 +28,7 @@
 #include "AP_BattMonitor_Synthetic_Current.h"
 #include "AP_BattMonitor_AD7091R5.h"
 #include "AP_BattMonitor_Scripting.h"
+#include "AP_BattMonitor_VEDirect.h"
 
 #include <AP_HAL/AP_HAL.h>
 
@@ -707,6 +708,11 @@ AP_BattMonitor::init()
                 drivers[instance] = NEW_NOTHROW AP_BattMonitor_INA3221(*this, state[instance], _params[instance]);
                 break;
 #endif  // AP_BATTERY_INA3221_ENABLED
+#if AP_BATTERY_VEDIRECT_ENABLED
+            case Type::VEDirect:
+                drivers[instance] = NEW_NOTHROW AP_BattMonitor_VEDirect(*this, state[instance], _params[instance]);
+                break;
+#endif  // AP_BATTERY_VE_DIRECT_ENABLED
             case Type::NONE:
             default:
                 break;
